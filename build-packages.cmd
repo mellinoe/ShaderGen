416361b6578bf13af162624cbacae9262d65107f
Eric Mellino <erme@microsoft.com>
2017-09-22 01:19:07 -0700

    Add ShaderGen.csproj to the list of packages being produced.

--- build-packages.cmd
+++ build-packages.cmd
@@ -1,3 +1,4 @@
 @echo off
 dotnet pack src\ShaderGen.Primitives\ShaderGen.Primitives.csproj -c Release
+dotnet pack src\ShaderGen\ShaderGen.csproj -c Release
 dotnet pack src\ShaderGen.Build\ShaderGen.Build.csproj -c Release
--- build-packages.sh
+++ build-packages.sh
@@ -1,2 +1,3 @@
 dotnet pack src/ShaderGen.Primitives/ShaderGen.Primitives.csproj -c Release
+dotnet pack src/ShaderGen/ShaderGen.csproj -c Release
 dotnet pack src/ShaderGen.Build/ShaderGen.Build.csproj -c Release
