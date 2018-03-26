94df88b95dad5ee0ae80ddc259dce23aa94356b7
Eric Mellino <mellinoe@gmail.com>
2018-03-26 14:59:45 -0700

    Fix address space declaration for by-ref Metal parameters.

--- src/ShaderGen/Metal/MetalMethodVisitor.cs
+++ src/ShaderGen/Metal/MetalMethodVisitor.cs
@@ -34,7 +34,8 @@ namespace ShaderGen.Metal
 
         protected override string FormatParameter(ParameterDefinition pd)
         {
-            return $"{_backend.CSharpToShaderType(pd.Type.Name)} {_backend.ParameterDirection(pd.Direction)}{_backend.CorrectIdentifier(pd.Name)}";
+            string addressSpace = pd.Direction == ParameterDirection.In ? string.Empty : "thread";
+            return $"{addressSpace} {_backend.CSharpToShaderType(pd.Type.Name)} {_backend.ParameterDirection(pd.Direction)}{_backend.CorrectIdentifier(pd.Name)}";
         }
     }
 }
