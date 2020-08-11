c1c6a8447c20cc131724b1b52df3a50d93146528
onehundredfeet <rcleven@mac.com>
2020-08-10 22:56:07 -0700

    Reverted test reversals

--- src/ShaderGen.Tests/ShaderSetDiscovererTests.cs
+++ src/ShaderGen.Tests/ShaderSetDiscovererTests.cs
@@ -23,11 +23,11 @@ namespace ShaderGen.Tests
             ShaderGenerator sg = new ShaderGenerator(compilation, backend);
             ShaderGenerationResult generationResult = sg.GenerateShaders();
             IReadOnlyList<GeneratedShaderSet> hlslSets = generationResult.GetOutput(backend);
-            //Assert.Equal(hlslSets.Count, 4 ); // I'm not sure this count is accurate
+            Assert.Equal(4, hlslSets.Count);
             GeneratedShaderSet set = hlslSets[0];
             
             //Updated to new naming convention
-            Assert.Equal(set.Name, set.VertexFunction.Name + "+" + set.FragmentFunction.Name );
+            Assert.Equal(set.VertexFunction.Name + "+" + set.FragmentFunction.Name, set.Name);
 
             CompileResult result = toolChain.Compile(set.VertexShaderCode, Stage.Vertex, "VS");
             Assert.False(result.HasError, result.ToString());
